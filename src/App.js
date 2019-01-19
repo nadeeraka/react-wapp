6da4aa82ca5307d93f358c8713ce10b7759c961a
nadeeraka <nimanthanadeeraka@outlook.com>
2019-01-19 13:35:13 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import Title from "./components/title";
+import API_KEY from "./util/secrets";
+//`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <p>{API_KEY}</p>
+        <Title />
       </div>
     );
   }
---
+++ src/components/title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div>
+      <h1 className="text-center">Weather App</h1>
+      <h3>Simple and esy to use util app</h3>
+    </div>
+  );
+};
+export default Title;
