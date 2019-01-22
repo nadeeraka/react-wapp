2e9b4e5fe22a7aeccb568e6208194c12e28a784c
nadeeraka <nimanthanadeeraka@outlook.com>
2019-01-22 13:14:22 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import Title from "./components/title";
+import Form from "./components/form";
 import API_KEY from "./util/secrets";
-//`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+import axios from "axios";
+
 class App extends Component {
+  getWhether = async () => {
+    const country = "sri lanka";
+    const city = "bulathsinhala";
+    const apiCall = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+    console.log(apiCall);
+  };
   render() {
     return (
       <div className="container">
         <p>{API_KEY}</p>
         <Title />
+        <Form />
       </div>
     );
   }
---
+++ src/components/form.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <div>
+      <form action="">
+        <div className="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            are your email with anyone else.
+          </small>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default Form;
